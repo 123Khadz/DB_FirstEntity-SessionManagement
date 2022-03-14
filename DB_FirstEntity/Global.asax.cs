87f3adefbc3f057c822e8272b483c9186e806964
khadi <khadi@DESKTOP-J7PQMCI>
2022-03-14 15:19:59 +0530

    Add project files.

---
+++ DB_FirstEntity/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace DB_FirstEntity
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Application["totalvisitor"] = 0;//100000
+        }
+
+        protected void Session_Start(object sender, EventArgs arg)
+        {
+            Application.Lock();
+            Application["totalvisitor"] = Convert.ToInt32(Application["totalvisitor"]) + 1;
+            Session["UserName"] = null;//Registering session key with current session
+            Session["LoginTime"] = null;
+            Application.UnLock();
+        }
+
+        //Application_End()
+        //Session_Start()
+        //Application_Error()
+        //Session_End()
+
+
+    }
+}
